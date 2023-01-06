5031ace0750ce6fc79d6d92e5628f369eab4d020
pietromusci <110541993+pietromusci@users.noreply.github.com>
2023-01-06 23:50:56 +0100

    Script Exit Button & Pulizia Hierachry
    //

---
+++ My project (3)/Assets/Scripts/UIScene.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if (UNITY_EDITOR)
+using UnityEditor;
+#endif
+public class UIScene : MonoBehaviour
+{
+    public void OnExitButtonClick()
+    {
+#if(UNITY_EDITOR)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+#else
+{
+Application.Quit;
+}
+#endif
+    }
+}
