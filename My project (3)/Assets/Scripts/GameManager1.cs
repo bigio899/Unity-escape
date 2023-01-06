ebc4a5385bf2d3e88a673d2debeb3846eba79d59
pietromusci <110541993+pietromusci@users.noreply.github.com>
2023-01-06 20:48:02 +0100

    Loading Coroutine & Pulizia Codice/Hierachry
    //

---
+++ My project (3)/Assets/Scripts/GameManager1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager1 : MonoBehaviour
+{
+    private bool isCoroutineEnded = false;
+    [SerializeField] GameObject loadingSubScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingSubScene.gameObject.SetActive(true);
+        StartCoroutine(LoadingCoroutine());
+    }
+    private void Update()
+    {
+        if(isCoroutineEnded==true)
+        {
+            loadingSubScene.gameObject.SetActive(false);
+        }
+        
+    }
+
+    public IEnumerator LoadingCoroutine()
+    {
+        Debug.Log("Inizio Coroutine");
+        yield return new WaitForSeconds(3);
+        isCoroutineEnded = true;
+        Debug.Log("Fine Coroutine");
+    }
+
+}
--- My project (3)/Assets/Scripts/OptionsGameObject.cs
+++ My project (3)/Assets/Scripts/OptionsGameObject.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OptionsGameObject : MonoBehaviour
 {
-    [SerializeField] GameObject menuoptions;
-    [SerializeField] GameObject menumain;
+    private int levelAvancement=1;
+    //declaration gameobjects's variables 
+    [SerializeField] GameObject optionsMenu;
+    [SerializeField] GameObject mainMenu;
+    //function that is performed when the player click the Option button. 
     public void OnClickOptionButton()
     {
-        menumain.gameObject.SetActive(false);
-        menuoptions.gameObject.SetActive(true);
+        mainMenu.gameObject.SetActive(false);
+        optionsMenu.gameObject.SetActive(true);
+    }
+    //function that is performed when the player click the Back To Menu button from the option setting.
+    public void OnClickBackToMenu()
+    {
+        mainMenu.gameObject.SetActive(true);
+        optionsMenu.gameObject.SetActive(false);
+    }
+    //function that is performed then the player click the Play button.
+    public void ChangeSceneFromMenuToLevel()
+    {
+        SceneManager.LoadScene(levelAvancement);
     }
 }
